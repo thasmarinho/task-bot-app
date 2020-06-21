ef56638d6e355e1f79f2d1f5eb1a7ede9387bbf1
Thais Marinho <thasmarinho@gmail.com>
2020-06-20 21:47:17 -0300

    Create rails project

---
+++ db/migrate/20200621004122_create_robots.rb
@@ -0,0 +1,10 @@
+class CreateRobots < ActiveRecord::Migration[6.0]
+  def change
+    create_table :robots do |t|
+      t.string :name
+      t.string :slogan
+
+      t.timestamps
+    end
+  end
+end
